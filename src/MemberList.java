469e884f93de65efab2215392436778c88285601
LatinaEnjoyer <nioe1000@stud.ek.dk>
2025-11-26 12:46:46 +0100

    init

---
+++ src/MemberList.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+public class MemberList {
+    private ArrayList<Member> allMembers = new ArrayList<>();
+    private ArrayList<Member> under18 = new ArrayList<>();
+    private ArrayList<Member> over18 = new ArrayList<>();
+    private final Member member;
+
+    MemberList(Member member){
+        this.member = member;
+    }
+
+    public int calculateAge(LocalDate birtdate){
+        Period currentAge = Period.between(birtdate, LocalDate.now());
+        return currentAge.getYears();
+    }
+
+    public static void main(String[] args) {
+        Member t = new Member();
+        MemberList n = new MemberList(t);
+        System.out.println("Age of person is "+n.calculateAge(LocalDate.of(2004, 4, 4)));
+    }
+}
