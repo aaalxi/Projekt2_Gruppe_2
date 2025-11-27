8170ce8082cfeaa69a6e5b87a432f5a3eac4729d
RwR92 <ruro0001@stud.ek.dk>
2025-11-27 13:20:09 +0100

    Rettet kode efter merge med main.

--- src/MemberFileHandling.java
+++ src/MemberFileHandling.java
@@ -6,7 +6,7 @@ public class MemberFileHandling {
 
     //Lav metoder så medlemmer kan gemmes og loades lokalt fra fil ved system opstart.
     public static void saveMembers(ArrayList<Member> members, String fileName) {
-        ArrayList<Member> exsistingMembers = loadMembers(fileName);
+        ArrayList<Member> exsistingMembers = loadMembers(fileName); //Liste af medlemmer som allerede er i txt filen.
         try (PrintWriter pw = new PrintWriter(new FileWriter(fileName, true))) {
             for (Member m : members) {
                 boolean exists = false;
@@ -19,8 +19,10 @@ public class MemberFileHandling {
                 if (!exists) {
                     String linje = m.getMemberID() + ";" + m.getName() + ";" + m.getDateOfBirth() + ";" + m.getIsCompetitive() + ";" + m.getTotalArrears();
                     pw.println(linje);
+                    exsistingMembers.add(m);
                 }
             }
+            System.out.println("Filen: ("+fileName+") er gemt.");
         } catch (IOException e) {
             System.out.println("Fejl ved gemning: " + e.getMessage());
         }
@@ -28,8 +30,7 @@ public class MemberFileHandling {
 
     public static ArrayList<Member> loadMembers(String fileName) {
         ArrayList<Member> members = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
             String linje;
             while ((linje = br.readLine()) != null) {
                 String[] data = linje.split(";");
@@ -46,7 +47,6 @@ public class MemberFileHandling {
                     members.add(new Casual(id, name, dateBirth, totalArrears));
                 }
             }
-            br.close();
         } catch (IOException e) {
             System.out.println("Fejl ved læsning: " + e.getMessage());
         }
