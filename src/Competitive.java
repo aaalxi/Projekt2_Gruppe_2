350f962c947800f12015123cd170b6994397eae3
RwR92 <ruro0001@stud.ek.dk>
2025-12-01 15:55:21 +0100

    init commit

--- src/Competitive.java
+++ src/Competitive.java
@@ -1,9 +1,16 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Competitive extends Member {
+
+    private final ArrayList<Discipline> disciplines = new ArrayList<>();
 
     public Competitive(String memberID, String name, LocalDate dateBirth, double totalArrears) {
         super(memberID, name, dateBirth, totalArrears);
         isCompetitive = true;
+    }
+
+    public void addDiscipline(Discipline discipline){
+        if (!)
     }
 }
