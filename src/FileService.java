ba64c9050816bab33eff5d48ea8868abaeb7cf41
jshar <joha0013@stud.ek.dk>
2025-12-10 14:10:05 +0100

    ændring af medlemstype

---
+++ src/FileService.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class FileService {
+    public static Member findByID(String memberID, ArrayList<Member> allMembers){
+        if (memberID == null || memberID.isBlank()) return null;  // tjek input
+        for (Member m : allMembers) {
+            if (m.getMemberID().equalsIgnoreCase(memberID)) { // ignoreCase for fleksibilitet
+                return m;  // medlem fundet
+            }
+        }
+        return null;  // medlem ikke fundet
+    }
+}
